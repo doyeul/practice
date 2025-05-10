20e8c3c0c654c845ecc6e357699c528b9e93c35e
doyeul <mark01100070@gmail.com>
2025-05-10 16:53:33 +0900

    Add files via upload
    std::cout.width is applied to the next output only, and resets after each insertion.

---
+++ width.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+
+	std::cout.width(4);
+	std::cout<<"*"<<std::endl;
+	std::cout.width(4);
+	std::cout<<"**"<<std::endl;
+	std::cout.width(4);
+	std::cout<<"***"<<std::endl;
+	std::cout.width(4);
+	std::cout<<"****"<<std::endl;
+
+	
+	return 0;
+	
+
+}
